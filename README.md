88307c9ef2680dd1731a3a4d7175e1397f32e649
ruanazevedo <ruann.azevedo@outlook.com>
2022-09-01 17:32:26 -0300

    inicio da abstracao correta da calculadora (incompleto)

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# APOOInCpp
+# Ánalise de Projetos Orientado à Objeto
+
+Professor **Lucio Valentim**
+
+Projeto para disciplina de APOO do curso de **Bacharelado em Ciência da Computação**.
+
+Universidade Tecnológica Federal do Paraná - Campo Mourão.
---
+++ main.cpp
@@ -0,0 +1,54 @@
+
+enum Digit { ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGTH, NINE };
+enum Signal { POSITIVE, NEGATIVE };
+enum Operator { ADD, SUBTRACT, MULTIPLY, DIVIDE };
+enum Controller { EQUAL, CLEAR };
+
+class Display {
+    public:
+        void addDigit(Digit digit);
+        void clear();
+};
+
+class DsiplayRuan : public Display {
+    // TODO fazer algoritmo do display
+};
+
+class Key {
+    public:
+        void press();
+        void setKeyboard(Keyboard keyboard);
+};
+
+class Keyboard {
+    public:
+        Key findKey(char c);        
+        void receiveDigit(Digit digit);
+        void setCpu(Cpu cpu);
+};
+
+
+class Cpu {
+    public:
+        void receiveDigit(Digit digit);
+        void setDisplay(Display display);
+};
+
+class Calculator {
+    public:
+        void setDisplay(Display display);
+        void setKeyboard(Keyboard keyboard);
+        void setCpu(Cpu cpu);
+};
+
+int main() {
+
+    Calculator calc;
+
+    /*calc.getKeyboard().findKey('2').press();
+    calc.getKeyboard().findKey('+').press();
+    calc.getKeyboard().findKey('2').press();
+    calc.getKeyboard().findKey('=').press();*/
+
+    return 0;
+}
